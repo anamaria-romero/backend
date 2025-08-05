7ce33a4f3ffe3bedafdb1d88cfc6f6d5a77fda80
anamaria-romero <anamrome16@gmail.com>
2025-08-05 14:20:02 -0500

    Guardado

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/adminController.js');
+const adminController = require('../controllers/AdminController.js');
 
 router.get('/', adminController.obtenerAdministradores);
 router.post('/', adminController.registrarAdministrador);
